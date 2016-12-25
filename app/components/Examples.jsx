5deee2e6c6c8838447a7efab8a22c5fe9e89c071
tsvetkovpro <285019@gmail.com>
2016-12-25 11:29:13 +0500

    fix heroku deploy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,12 @@
-var React = require( 'react' );
+var React = require('react');
 
-
-var Examples = ( props ) => {
-	return (
-		<h3>Examples component</h3>
-	)
+var Examples = (props) => {
+  return (
+    <div>
+      <h3>Examples Component!</h3>
+      <p>Welcome to examples page!</p>
+    </div>
+  )
 };
 
 module.exports = Examples;
--- example-promise.js
+++ example-promise.js
@@ -28,23 +28,23 @@
 
 // Challenge Area
 function addPromise (a, b) {
-	return new Promise(function (resolve, reject) {
-		if (typeof a === 'number' && typeof b === 'number') {
-			resolve(a + b);
-		} else {
-			reject('A & b need to be numbers');
-		}
-	});
+  return new Promise(function (resolve, reject) {
+    if (typeof a === 'number' && typeof b === 'number') {
+      resolve(a + b);
+    } else {
+      reject('A & b need to be numbers');
+    }
+  });
 }
 
 addPromise(2, 3).then(function (sum) {
-	console.log('success', sum);
+  console.log('success', sum);
 }, function (err) {
-	console.log('error', err);
+  console.log('error', err);
 });
 
 addPromise('andrew', 9).then(function (sum) {
-	console.log('this should not show up');
+  console.log('this should not show up');
 }, function (err) {
-	console.log('This should appear', err);
+  console.log('This should appear', err);
 });
